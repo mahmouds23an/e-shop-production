5c0f5747996549cdadf7a530e92ee85b6ec347c2
mahmouds23an <pgemahmoud@gmail.com>
2025-03-02 11:42:45 +0200

    some new edits

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -35,7 +35,9 @@ const placeOrderStripe = async (req, res) => {};
 
 const allOrders = async (req, res) => {
   try {
-    const orders = await orderModel.find({}).sort({ date: -1 });
+    const orders = await orderModel
+      .find({ status: { $ne: "Cancelled" } })
+      .sort({ date: -1 });
     if (!orders) {
       return res
         .status(404)
