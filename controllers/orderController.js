7d89501f59a7f7e51d5ef75e63c80394154841d4
mahmouds23an <pgemahmoud@gmail.com>
2025-03-04 03:54:34 +0200

    some new edits

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -35,7 +35,7 @@ const placeOrderStripe = async (req, res) => {};
 
 const allOrders = async (req, res) => {
   try {
-    const orders = await orderModel.sort({ date: -1 });
+    const orders = await orderModel.find().sort({ date: -1 });
     if (!orders) {
       return res
         .status(404)
